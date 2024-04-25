b2eeb0ee26058bdf6740ce508e7fe59bafbcef15
RedaQal <redaqal@gmail.com>
2024-04-25 18:46:43 +0100

    create seeder for Employe

---
+++ database/seeders/EmployeSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employe;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class EmployeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table(
+            'employes'
+        )->insert(
+            [
+                'name' => 'admin',
+                'role' => 'admin',
+                'email' => 'admin@admin',
+                'password' => bcrypt('admin'),
+                'tel' => "0611111111",
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
+    }
+}
